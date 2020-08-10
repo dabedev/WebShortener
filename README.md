57d180b2949fe53e4330ae91023342156bb949c6
DannyyDev <42282680+DannyyDev@users.noreply.github.com>
2020-08-10 14:34:38 -0400

    Update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# urlshortener
-URL shortener service.
+# WebShortener
+URL shortener web service.
+
+## Developed with:
+> - HTML5
+> - JavaScript
+> - NodeJS
+> - Express
+> - SQLite
+
+## Features
+
+- [x] Shortened URL redirect
+- [x] Auth system
+- [x] Custom URL
+
+## TODO
+
+- [ ] Encrypt user data
+- [ ] Random URL generator
+- [ ] Advanced user panel
+- [ ] Better design
+
+
+**DISCLAIMER**: The distributor of this software isn't responsible for any information that may be leaked from this site either for any loss this can cause for you or your company. 
+
+## Discord Server
+<p align="center">
+  <a href="https://discord.gg/gHGyF2B"><img src="https://discordapp.com/api/guilds/737207462519636029/widget.png?style=banner2" alt="Discord server"></a>
+</p>
--- script/index.js
+++ script/index.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const app = express();
-app.listen(7777);
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const urlDB = new sqlite3.Database('./modules/databases/shortenurls.sqlite');
+const usersDB = new sqlite3.Database('./modules/databases/users.sqlite');
+urlDB.run("CREATE TABLE IF NOT EXISTS urlsData(userid TEXT, shortenURL TEXT, toRedirectURL TEXT, visited INT)");
+usersDB.run("CREATE TABLE IF NOT EXISTS usersData(userid TEXT, email TEXT, password TEXT, token TEXT)");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlenconded({extended: true}));
+
 app.get("/", function(req, res){
 res.sendStatus(200);
 });
+
+app.get("/user/:TOKEN", function(req, res){
+
+});
+
+app.get("/url/:shortenURL", function(req, res){
+
+});
+
+app.post("/urls/add", function(req, res){
+
+});
+
+app.post("/auth/register", function(req, res){
+
+});
+
+app.post("/auth/login", function(req, res){
+
+});
+
+app.listen(7777);
